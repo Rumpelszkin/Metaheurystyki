9d2d74853d887b7c94ece358fd9562ba9dabd2c7
Rumpelszkin <kolbuszewskimax@gmail.com>
2018-10-07 21:31:26 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Loader xd = new Loader();
+        xd.load("C:\\Users\\Max\\Desktop\\Study\\Metaheurystyki\\ttp_student\\easy_0.ttp");
+
     }
 }
